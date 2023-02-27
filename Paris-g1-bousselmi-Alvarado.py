5721ead3ec5f3d8e157df6c168de811e1a613aca
rebecaalvarado <126494236+rebecaalvarado@users.noreply.github.com>
2023-02-27 17:39:40 +0100

    exercise 1

--- Paris-g1-bousselmi-Alvarado.py
+++ Paris-g1-bousselmi-Alvarado.py
@@ -1,10 +1,4 @@
-#!/usr/bin/env python3
-# -*- coding: utf-8 -*-
-"""
-Created on Mon Feb 27 16:36:55 2023
 
-@author: rebeca
-"""
 
 print( "hello from - rebeca")
 print ("hello")
@@ -14,3 +8,19 @@ print ("hola I'm Anthony")
 print("rajoute")
 
 print("tryout 1")
+
+import seaborn as sns
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+
+
+#HEATMAP, Exercise 1.-------------------------------------------------------------
+# Create a dataset
+df = pd.DataFrame(np.random.random((5,5)), columns=["a","b","c","d","e"])
+# numpy.random.random() is one of the function for doing random sampling in numpy. 
+#It returns an array of specified shape and fills it with random floats in the half-open interval [0.0, 1.0).
+
+
+# Default heatmap            ------- plot first heatmap
+p1 = sns.heatmap(df)
